22d3253bcbddc7b5e6770913f769b98449a02011
tianyu5177 <zhangtianyu5177@sina.com>
2020-11-23 09:48:38 +0800

    更新

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import {sub,sum} from './module1'
+import {data,message} from './module2'
+import school from './module3'
+
+sum(1,2)
+sub(3,4)
+console.log(data);
+console.log(message);
+console.log(school);
---
+++ src/js/module1.js
@@ -0,0 +1,8 @@
+//module1使用分别暴露
+export function sum(a,b) {
+	console.log(a+b);
+}
+
+export function sub(a,b) {
+	console.log(a-b);
+}
